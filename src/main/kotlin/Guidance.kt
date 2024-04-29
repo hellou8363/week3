85d09dfd469fd9cd2876e486baf12ca1ffa4c35f
eunhye han <hellou8363@gmail.com>
2024-04-29 16:20:27 +0900

    ✨ 안내 메시지만 모아놓은 클래스 추가

---
+++ src/main/kotlin/Guidance.kt
@@ -0,0 +1,19 @@
+package org.example
+
+class Guidance {
+
+    fun displayGreeting() {
+        println("[          숫자 야구 게임          ]")
+        println("환영합니다! 원하시는 번호를 입력해주세요.")
+        println("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기")
+    }
+
+    fun displayMenu() {
+        println("\n원하시는 번호를 입력해주세요.")
+        println("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기")
+    }
+
+    fun displayExit() {
+        println("< 숫자 야구 게임을 종료합니다. >")
+    }
+}
