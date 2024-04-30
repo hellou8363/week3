ca7ddb296c2959dc6e5f55b8e3b5db5d81c0e66d
eunhye han <hellou8363@gmail.com>
2024-04-30 10:29:20 +0900

    ♻️ Game 클래스의 play 함수는 값을 비교해서 결과를 주는 역할만 하도록 변경

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -2,51 +2,27 @@ package org.example
 
 class Game {
 
-    fun play() {
-        val storage = Storage()
-        val randomNumber = RandomNumber().generateThreeRandomNumbers()
-        var count = 0
+    fun play(value: List<Int>, userAnswer: List<Int>): String {
+        val strike = value.filterIndexed { index, c -> c == userAnswer[index] }.size
+        val ball = value.filter { userAnswer.contains(it) }.size - strike
+        var result = ""
 
-        println("< 게임을 시작합니다. >")
-        println("숫자를 입력하세요")
+        if (strike > 0) {
+            result += "${strike}스트라이크 "
 
-        while (true) {
-            val input = readln()
-            count++
-
-            try {
-                val value = Validation.validateInputValue(input).map { Character.getNumericValue(it) }
-                var result = ""
-                val strike = value.filterIndexed { index, c -> c == randomNumber[index] }.size
-                val ball = value.filter { randomNumber.contains(it) }.size - strike
-
-                if (strike > 0) {
-                    result += "${strike}스트라이크 "
-
-                    if (strike == 3) {
-                        println("정답입니다!\n")
-
-                        storage.save(count)
-                        break
-                    }
-                }
-
-                if (ball > 0) {
-                    result += "${ball}볼 "
-                }
-
-                if (strike == 0 && ball == 0) {
-                    result = "Nothing"
-                }
-
-                println(result)
-
-            } catch (e: NumberFormatException) {
-                println(e.message)
-            } catch (e: Exception) {
-                println(e.message)
+            if (strike == 3) {
+                return "정답입니다!"
             }
         }
 
+        if (ball > 0) {
+            result += "${ball}볼 "
+        }
+
+        if (strike == 0 && ball == 0) {
+            result = "Nothing"
+        }
+
+        return result
     }
 }
