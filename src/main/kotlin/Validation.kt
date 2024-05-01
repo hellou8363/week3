9e25652bea3dc2c563c6e56925ddf6f941af31d0
eunhye han <hellou8363@gmail.com>
2024-05-01 21:10:08 +0900

    ♻️ Game 클래스의 paly 함수를 Validation 클래스로 이동, play -> checkAnswer로 함수명 변경

--- src/main/kotlin/Validation.kt
+++ src/main/kotlin/Validation.kt
@@ -28,4 +28,28 @@ class Validation { // 유효값 검증을 위한 클래스
 
         return value
     }
+
+    fun checkAnswer(value: List<Int>, userAnswer: List<Int>): String {
+        val strike = value.filterIndexed { index, c -> c == userAnswer[index] }.size
+        val ball = value.filter { userAnswer.contains(it) }.size - strike
+        var result = ""
+
+        if (strike > 0) {
+            result += "${strike}스트라이크 "
+
+            if (strike == 3) {
+                return "정답입니다!"
+            }
+        }
+
+        if (ball > 0) {
+            result += "${ball}볼 "
+        }
+
+        if (strike == 0 && ball == 0) {
+            result = "Nothing"
+        }
+
+        return result
+    }
 }
